f4cf4fd1736e7fde37c27f4952a7b2afd31e76b6
Akhilreddy-GIT <kns.akhilreddy@gmail.com>
2026-07-09 11:49:30 +0530

    [347] [Time Beats: 61.65%] [Memory Beats: 80.34%] - LeetPush

---
+++ 347-top-k-frequent-elements/347-Top-K-Frequent-Elements.py
@@ -0,0 +1,26 @@
+#THIS IS MY BRUTE FORCE APPROACH IF YOU NEED OPTIMAL SOLUTION USING HEAP YOU CAN REFER THE SAME PROBLEM WITH SAME NAME AS THIS IN MY REPOSITORY
+
+
+from typing import List
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = {}
+
+        # Step 1: Count frequencies
+        for num in nums:
+            if num not in freq:
+                freq[num] = 1
+            else:
+                freq[num] += 1
+
+        # Step 2: Sort the dictionary items by frequency
+        sorted_freq = sorted(freq.items(), key=lambda x: x[1])
+
+        # Step 3: Take the last k elements and return only the numbers
+        result = []
+
+        for i in range(len(sorted_freq) - k, len(sorted_freq)):
+            result.append(sorted_freq[i][0])
+
+        return result
